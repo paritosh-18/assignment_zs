dd04deb6bd1d03679ad176001f7299c6f2553393
paritosh-18 <paritoshchauhan18@gmail.com>
2023-12-19 20:24:41 +0530

    Removed unnecessary files

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
-// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
